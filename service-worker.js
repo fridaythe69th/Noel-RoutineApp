e8c0657af14978898fe098dd03a21b775bb3597d
fridaythe69th <nikhil.noel8@gmail.com>
2025-08-12 12:26:33 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,22 @@
-const CACHE_NAME = 'adhd-routine-cache-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME = "routine-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./styles.css",
+  "./script.js"
 ];
 
-// Install event - cache files
-self.addEventListener('install', (event) => {
+// Install service worker and cache files
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch event - serve cached files if offline
-self.addEventListener('fetch', (event) => {
+// Serve cached files when offline
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
